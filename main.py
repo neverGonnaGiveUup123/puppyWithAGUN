bad772adc52fc8f0e63b9ce744d187830a1d4bb8
Siddharth <st22041@ormiston.school.nz>
2024-05-22 14:41:55 +1200

    Working collision detection. Little sketchy though

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import sys
 import pygame
 from settings import *
-# from src.sprites.puppy.puppy import Puppy
-# from src.sprites.bullet.bullet import Bullet
-# from src.sprites.enemies.baseEnemies import Drone
 from newSprites import Sprites
 
 pygame.init()
@@ -14,42 +11,11 @@ pygame.display.set_gamma(GAMMA[0],GAMMA[1],GAMMA[2])
 
 clock = pygame.time.Clock()
 
-# bulletImg = pygame.image.load("src/sprites/bullet/BULLET.png")
-
 SCREEN = pygame.display.set_mode(SCREENDIMENSIONS)
 
-# bullets = []
-
-# def handle_keyboard_input():
-#     keys = pygame.key.get_pressed()
-#     if any(keys):
-#         if keys[pygame.K_d]:
-#             puppy.move_right()
-#         if keys[pygame.K_s]:
-#             puppy.move_down()
-#         if keys[pygame.K_a]:
-#             puppy.move_left()
-#         if keys[pygame.K_w]:
-#             puppy.move_up()
-#         if keys[pygame.K_SPACE]:
-#             bullets.append(Bullet(24,5,pygame.image.load('src/sprites/bullet/BULLET.png'),SCREEN,15,5,puppy.return_pos(),pygame.mouse.get_pos()))
-
-#     puppy.update()
-    # print(puppy.return_pos())
-
-# enemies = []
-# enemies.append(Drone(4,4,pygame.image.load('src/sprites/bullet/BULLET.png'), (800,800), SCREEN, puppy.return_pos()))
-# def handle_sprites():
-#     # for i in enemies:
-#     # bullets.append(i.attack())
-    
-#     for i in bullets:
-#         if i.return_lifetime() == 0:
-#             bullets.remove(i)
-#         i.handle_bullet()
-# bullets.append(enemies[0].attack())
 Sprites.screen = SCREEN
-Sprites.Player(8, pygame.image.load(PLAYERSKINDEFAULT))
+player = Sprites.Player(16, pygame.image.load(PLAYERSKINDEFAULT))
+Sprites.Drone(pygame.image.load('BULLET.png'), [500,500], 4, player.return_player_pos())
 
 while True:
     SCREEN.fill((0,0,0))
@@ -60,5 +26,4 @@ while True:
             sys.exit(0)
 
     Sprites.sprite_loop()
-    # print(1)
     pygame.display.flip()
--- newSprites.py
+++ newSprites.py
@@ -5,45 +5,86 @@ class Sprites:
     screen = None
     
     class Drone:
-        def __init__(self) -> None:
+        def __init__(self, skin: pygame.Surface, spawn: list, velocity: int, target) -> None:
             Sprites.sprites.append(self)
-            self.skin = None
+            self.skin = skin
+            self.pos = spawn
+            self.velocity = velocity
+            self.target = target
+            self.mask = pygame.mask.from_surface(self.skin)
 
         def __repr__(self) -> str:
             return "I am a drone!"
         
+        def move_towards_target(self):
+            if self.pos[0] == self.target[0] and self.pos[1] == self.target[1]:
+                return
+
+            if self.pos[0] >= self.target[0]:
+                self.pos[0] -= self.velocity
+            else:
+                self.pos[0] += self.velocity
+            
+            if self.pos[1] >= self.target[1]:
+                self.pos[1] -= self.velocity
+            else:
+                self.pos[1] += self.velocity
+
         def update(self):
-            pass
+            self.move_towards_target()
+            Sprites.screen.blit(self.skin, self.pos)
     
     class Player:
         def __init__(self, velocity: int, skin: pygame.Surface) -> None:
             Sprites.sprites.append(self)
             self.velocity = velocity
             self.skin = skin
-            self.playerPos = [0,0]
+            self.pos = [0,0]
+            self.cooldown = 20
+            self.mask = pygame.mask.from_surface(self.skin)
 
         def __repr__(self) -> str:
             return "I am the player!"
         
         def return_player_pos(self):
-            return self.playerPos
+            return self.pos
+        
+        def check_collisions(self):
+            collision = False
+            for spr in Sprites.sprites:
+                x_offset = spr.pos[0] - self.pos[0]
+                y_offset = spr.pos[1] - self.pos[1]
+                e = self.mask.overlap_area(spr.mask, (x_offset, y_offset))
+                print(collision)
+                print(e)
+                if e == 0:
+                    collision = False
+                elif e > 0 and collision == False:
+                    collision = True
+                elif e > 0 and collision == True:
+                    print("Collision!")
+
         
         def update(self):
+            self.check_collisions()
             # print("E")
             self.keys = pygame.key.get_pressed()
             if self.keys[pygame.K_w]:
-                self.playerPos[1] -= self.velocity
+                self.pos[1] -= self.velocity
             if self.keys[pygame.K_s]:
-                self.playerPos[1] += self.velocity
+                self.pos[1] += self.velocity
             if self.keys[pygame.K_a]:
-                self.playerPos[0] -= self.velocity
+                self.pos[0] -= self.velocity
             if self.keys[pygame.K_d]:
-                self.playerPos[0] += self.velocity
+                self.pos[0] += self.velocity
             if self.keys[pygame.K_SPACE]:
-                self.bullet = Sprites.Bullet(self.playerPos, pygame.mouse.get_pos(), 8, pygame.image.load('BULLET.png'), 60)
-                Sprites.sprites.append(self.bullet)
+                if self.cooldown <= 0:
+                    bullet = Sprites.Bullet(self.pos, pygame.mouse.get_pos(), 8, pygame.image.load('BULLET.png'), 60)
+                    Sprites.sprites.append(bullet)
+                    self.cooldown = 20
             
-            Sprites.screen.blit(self.skin, self.playerPos)
+            Sprites.screen.blit(self.skin, self.pos)
+            self.cooldown -= 1
 
     class Bullet:
         def __init__(self, startCoord: list, endCoord: tuple, velocity: int, skin: pygame.Surface, lifetime: int):
@@ -64,14 +105,10 @@ class Sprites:
             print(self.absGradient)
         
         def __repr__(self) -> str:
-            return "I am a bullet"
+            return str(self.lifetime)
         
         def update(self):
             self.lifetime -= 1
-            if self.lifetime <= 0:
-                Sprites.sprites.remove(self)
-                del self
-                return
 
             if self.absGradient <= 1:
                 if self.endCoord[0] > self.startCoord[0]:
@@ -89,9 +126,15 @@ class Sprites:
                     self.currentPos[0] = (self.currentPos[1] - self.c) / self.gradient
 
             Sprites.screen.blit(self.skin, self.currentPos)
-    
+
+            if self.lifetime <= 0:
+                Sprites.sprites.remove(self)
+                print("removed")
+
     @staticmethod
     def sprite_loop():
         for spr in Sprites.sprites:
-            # print(spr)
+            #print(spr)
             spr.update()
+            #print(spr)
+        #print("-------------")
